707dc45d1a9420a67c3d21aa058efe7f728613ec
Olexiy-repin <lyoharepin@gmail.com>
2018-02-06 18:14:47 +0200

    optimize images

--- src/js/modal.js
+++ src/js/modal.js
@@ -35,3 +35,18 @@
     });
   });
 })();
+
+(() => {
+    $('.sec-12__list-item').on('click', (event) => {
+      if ($(event.target).hasClass('more-btn')) {
+        $(event.currentTarget).find('.more-block').addClass('show');
+
+        $(document).mouseup(function (e) {
+          var container = $(".more-block");
+          if (container.has(e.target).length === 0){
+              container.addClass('hide');
+            }
+          });
+      }
+    });
+})();
